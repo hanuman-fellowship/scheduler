2b87296753c2a963e7a97706c3c8abc0dfe30eac
Jason Galuten <jason@galuten.com>
2010-07-06 23:01:38 -0700

    refined progress

--- webroot/progress.php
+++ webroot/progress.php
@@ -10,10 +10,11 @@ $data = explode('|',$data);
 $message = $data[0];
 $percent = $data[1];
 ?>
-
+<? if ($file) { ?>
 <center><?=$message;?><br>
 <div style='border:6px double #ccc;width:200px;margin-top:15px;padding:0;height:14px;'>
 <div id='progress_bar' style='float:left;color:#FFF;background-color:#FF8D40;height:12px;padding-bottom:2px;font-size:12px;text-align:center;overflow:hidden;line-height:1.2em;width:<?=$percent;?>%'>
 </div>
 </div>
 </center>
+<? } ?>
