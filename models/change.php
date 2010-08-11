f8663bcacda0549f195ee1229af6aac20aabc24e
Jason Galuten <jason@galuten.com>
2010-08-10 23:49:56 -0700

    applyChange is faster

--- models/change.php
+++ models/change.php
@@ -151,19 +151,23 @@ class Change extends AppModel {
 		); 
 		$this->ChangeModel->create(); 
 		$this->ChangeModel->save($change_model_data); 
+		$last_id = $this->ChangeModel->getLastInsertId();
+		$values = '';
 		foreach ($fields as $field_key) { 
-			$this->ChangeField->create(); 
-			$this->ChangeField->save(array(
-				'ChangeField' => array(
-					'change_id'       => 0, 
-					'change_model_id' => $this->ChangeModel->getLastInsertId(), 
-					'field_key'       => $field_key, 
-					'field_old_val'   => $this->oldData[$model_name][$field_key],
-					'field_new_val'   => $this->newData[$model_name][$field_key],
-					'schedule_id'     => $this->schedule_id
-				)
-			)); 
+			$values .= "(
+				'0',
+				'{$last_id}',
+				'{$field_key}',
+				'{$this->oldData[$model_name][$field_key]}',
+				'{$this->newData[$model_name][$field_key]}',
+				'{$this->schedule_id}'
+			),";
 		}
+		$values = substr_replace($values,'',-1);
+		$this->ChangeField->query("INSERT INTO change_fields
+			(change_id,change_model_id,field_key,field_old_val,field_new_val,schedule_id)
+			VALUES {$values}
+		");
         $this->clearHanging();
     }     
      
