e82aea26aa4df9e8bddef1c0a5b0a9383349d1ae
Jason Galuten <jason@galuten.com>
2010-08-15 15:47:18 -0700

    fixed add person

--- models/person.php
+++ models/person.php
@@ -24,10 +24,6 @@ class Person extends AppModel {
 		return true;
 	}
 
-	function beforeSave() {
-		debug($this-data);
-	}
-
 	function sSave($data) {
 		$resident_category_id = $data['Person']['resident_category_id'];
 		unset($data['Person']['resident_category_id']);
