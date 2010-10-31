99d6b72d30957fce519c58e83e2a13ff7a1fa9b9
Jason Galuten <jason@galuten.com>
2010-10-31 00:42:07 -0700

    added myDebug and timer

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,44 +1,26 @@
 <?php
-/* SVN FILE: $Id$ */
-/**
- * Short description for file.
- *
- * Long description for file
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @filesource
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.app.config
- * @since         CakePHP(tm) v 0.10.8.2117
- * @version       $Revision$
- * @modifiedby    $LastChangedBy$
- * @lastmodified  $Date$
- * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
- */
-/**
- *
- * This file is loaded automatically by the app/webroot/index.php file after the core bootstrap.php is loaded
- * This is an application wide file to load any function that is not used within a class define.
- * You can also use this to include or require any files in your application.
- *
- */
-/**
- * The settings below can be used to set additional paths to models, views and controllers.
- * This is related to Ticket #470 (https://trac.cakephp.org/ticket/470)
- *
- * $modelPaths = array('full path to models', 'second full path to models', 'etc...');
- * $viewPaths = array('this path to views', 'second full path to views', 'etc...');
- * $controllerPaths = array('this path to controllers', 'second full path to controllers', 'etc...');
- *
- */
-//EOF
+	
+function myDebug($var) {
+	echo "<div class='tall left'>";
+	debug($var);
+	echo "</div>";	
+}
+
+function timer($name = 0, $action) {
+    static $a;
+	static $total;
+	switch ($action) {
+		case 'start' :
+			$a[$name] = microtime(true);
+			return;
+		case 'stop' :
+			$b = microtime(true); 
+			$time = $b - $a[$name];
+			$a[$name] = $b;
+			$total[$name] = isset($total[$name]) ? $total[$name] + $time : $time;
+			return (string)$time;
+		case 'total' :
+			return (string)$total[$name];
+	}
+}
 ?>
