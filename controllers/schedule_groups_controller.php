9884dcf298c45933f89c81153a69c6090f238d03
Jason Galuten <jason@galuten.com>
2010-11-12 11:42:42 -0800

    fixed view past

--- controllers/schedule_groups_controller.php
+++ controllers/schedule_groups_controller.php
@@ -4,9 +4,13 @@ class ScheduleGroupsController extends AppController {
 	var $name = 'ScheduleGroups';
 
 	function select($id) {
-		$schedule_id = $this->ScheduleGroup->Schedule->field('id',array(
-			'Schedule.schedule_group_id' => $id
-		),array('Schedule.id desc'));
+		$schedule_id = $this->ScheduleGroup->Schedule->field('id',
+			array(
+				'Schedule.schedule_group_id' => $id,
+				'Schedule.name' => 'Published'
+			),
+			array('Schedule.id desc')
+		);
 		$this->setSchedule($schedule_id);
 		$this->redirect($this->referer());
 	}
