hanuman-fellowship/scheduler

commit 13a0405f99a381d973014f31e64c1de8f8b69a6d

print areas and people

controllers/people_controller.php
@@ -16,20 +16,24 @@ class PeopleController extends AppController {
 			} else {
 				$person = $this->Person->getPerson($id);
 				$this->Person->addDisplayName($person['Person']);
-				$this->Session->write('last_person',$id);
+				if (!isset($this->params['requested'])) {
+					$this->set('change_messages',$this->getChangeMessages());
+					$this->Session->write('last_person',$id);
+					$this->Person->PersonnelNote->order = 'PersonnelNote.order asc';
+					$personnelNotes = $this->Person->PersonnelNote->findAllByPersonId($id);
+					$this->set('personnelNotes', Set::combine(
+						$personnelNotes,'{n}.PersonnelNote.id','{n}.PersonnelNote.note'));
+					$this->Person->OperationsNote->order = 'OperationsNote.order asc';
+					$personnelNotes = $this->Person->OperationsNote->findAllByPersonId($id);
+					$this->set('operationsNotes', Set::combine(
+					$personnelNotes,'{n}.OperationsNote.id','{n}.OperationsNote.note'));
+				} else {
+					$this->set('print',true);
+				}
 			}
 			$this->set('person',$person);		
-			$this->set('change_messages',$this->getChangeMessages());
 			$this->set('bounds', $this->getBounds());
 			if ($id == 'gaps') $this->render('gaps');
-			$this->Person->PersonnelNote->order = 'PersonnelNote.order asc';
-			$personnelNotes = $this->Person->PersonnelNote->findAllByPersonId($id);
-			$this->set('personnelNotes', Set::combine(
-				$personnelNotes,'{n}.PersonnelNote.id','{n}.PersonnelNote.note'));
-			$this->Person->OperationsNote->order = 'OperationsNote.order asc';
-			$personnelNotes = $this->Person->OperationsNote->findAllByPersonId($id);
-			$this->set('operationsNotes', Set::combine(
-				$personnelNotes,'{n}.OperationsNote.id','{n}.OperationsNote.note'));
 		} else {
 			$this->redirect(array('action'=>'selectSchedule'));
 		}
@@ -154,6 +158,28 @@ class PeopleController extends AppController {
 				$this->redirect(array('action'=>'schedule',$goto['key']));
 			}
 		}
+	}
+
+	function printm($id = null) {
+		$output = '';
+		if (!empty($this->data)) {
+			foreach($this->data['Person'] as $category) {
+				if (is_array($category)) {
+					foreach($category as $id) {
+						$output .= $this->requestAction(
+							array('controller'=>'people','action'=>'schedule'),
+							array('pass'=>array($id),'return')
+						);
+					}
+				}
+			}
+			$this->set('output',$output);
+			$this->set('back',$this->referer());
+		} else {
+			$this->set('people',$this->Person->listByResidentCategory());
+			$this->data['Person']['person_id'] = array($id);
+			$this->render('select_print');
+		}	
 	}
 
 }

views/elements/menu.ctp
@@ -139,32 +139,40 @@
 		'shortcut' => 'ctrl+p',
 		'ajax',
 		'sub' => array(
-			'hidden' => $hidden,
 			'role' => array('operations'),
 			'View Schedule...' => array(
-				'role' => array('operations'),
+				'hidden' => $hidden,
 				'url' => array('controller' => 'people', 'action' => 'schedule'),
 				'ajax',
 				'shortcut' => 'ctrl+p'
 			),
-			"<hr/>",
+			array('hidden'=>$hidden,'title'=>"<hr/>"),
 			'New Person...' => array(
-				'role' => array('operations'),
+				'hidden' => $hidden,
 				'url' => array('controller' => 'people', 'action' => 'add'),
 				'ajax',
 				'shortcut' => 'shift+ctrl+p'
 			),
-			"<hr/>",
+			array('hidden'=>$hidden,'title'=>"<hr/>"),
 			'Restore Person...' => array(
-				'role' => array('operations'),
+				'hidden' => $hidden,
 				'url' => array('controller' => 'people', 'action' => 'restore'),
 				'ajax'
 			),
 			'Retire Person...' => array(
-				'role' => array('operations'),
+				'hidden' => $hidden,
 				'url' => array('controller' => 'people', 'action' => 'retire'),
 				'ajax'
-			)
+			),
+			array('hidden'=>$hidden,'title'=>"<hr/>"),
+			'Print People...' => array(
+				'url' => array(
+					'controller' => 'people',
+					'action' => 'printm',
+					(isset($person)) ? $person : null 
+				),
+				'ajax'
+			),
 		)
 	),
 	'Areas' => array(
@@ -172,22 +180,22 @@
 		'shortcut' => 'ctrl+a',
 		'ajax',
 		'sub' => array(
-			'hidden' => $hidden,
 			'role' => array('operations'),
 			'View Schedule...' => array(
+				'hidden' => $hidden,
 				'url' => array('controller' => 'areas', 'action' => 'select'),
 				'ajax',
 				'shortcut' => 'ctrl+a'
 			),
-			"<hr/>",
+			array('hidden'=>$hidden,'title'=>"<hr/>"),
 			'New Area...' => array(
-				'role' => array('operations'),
+				'hidden' => $hidden,
 				'url' => array('controller' => 'areas', 'action' => 'add'),
 				'ajax'
 			),
-			"<hr/>",
+			array('hidden'=>$hidden,'title'=>"<hr/>"),
 			'Clear Area...' => array(
-				'role' => array('operations'),
+				'hidden' => $hidden,
 				'url' => array(
 					'controller' => 'areas',
 					'action' => 'clear',
@@ -197,10 +205,19 @@
 				'shortcut' => 'ctrl+c'
 			),
 			'Delete Area...' => array(
-				'role' => array('operations'),
+				'hidden' => $hidden,
 				'url' => array(
 					'controller' => 'areas',
 					'action' => 'delete',
+					(isset($area)) ? $area : null 
+				),
+				'ajax'
+			),
+			array('hidden'=>$hidden,'title'=>"<hr/>"),
+			'Print Areas...' => array(
+				'url' => array(
+					'controller' => 'areas',
+					'action' => 'printm',
 					(isset($area)) ? $area : null 
 				),
 				'ajax'

views/elements/schedule_content.ctp
@@ -33,7 +33,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 		<td width="9" rowspan="2">
 			&nbsp;
 		</td> 
-		<td class="title" <?= (isset($person)) ? 'id="total_hours"' : '';?> width="144" rowspan="2">
+		<td class="title" <?= (isset($person)) ? 'id="total_hours_'.$person['Person']['id'].'"' : '';?> width="144" rowspan="2">
 			<?= (isset($area)) ?
 				$area[$request.'Area']['manager'] :
 				'';?>
@@ -259,7 +259,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 			<?// now that the total hours are added up, sneak them in at the top
 			?>
 			<script type="text/javascript">
-				$('total_hours').innerHTML = <?=$schedule->total_hours['total'];?>;
+				$('total_hours_<?=$person['Person']['id']?>').innerHTML = <?=$schedule->total_hours['total'];?>;
 			</script>
 		<? } ?>
 		<? if ($isOperations && $editable && !$request
@@ -313,7 +313,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 	</tr> 
 	<? } ?>	
 </table> 
-<? if (!$request && !$gaps) { ?>
+<? if (!$request && !$gaps && !isset($print)) { ?>
 	<table align='center' width='850px' style='font-size:17pt;text-align:center'>
 		<tr>
 			<td align='left'>
@@ -349,7 +349,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 	</tr>
 </table>
 <? } ?>
-<? if (($isPersonnel || $isOperations) && isset($person) && !$gaps) { ?>
+<? if (($isPersonnel || $isOperations) && isset($person) && !$gaps && !isset($print)) { ?>
 <table align="center" width="774px" style="position:relative;top:-30px">
 	<tr>
 		<td valign="top" style="font-size:14pt;text-align:left">
@@ -483,3 +483,4 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 	<? } ?>
 <? } ?>
 <?$schedule->displayHoursBy()?>
+<?$schedule->clearHours()?>

controllers/areas_controller.php
@@ -9,8 +9,12 @@ class AreasController extends AppController {
 			$this->redirectIfNotValid($id);
 			$this->set('area',$this->Area->getArea($id));
 			$this->set('bounds', $this->getBounds());
-			$this->set('change_messages',$this->getChangeMessages());
-			$this->Session->write('last_area',$id);
+			if (!isset($this->params['requested'])) {
+				$this->set('change_messages',$this->getChangeMessages());
+				$this->Session->write('last_area',$id);
+			} else {
+				$this->set('print',true);
+			}
 		} else {
 			$this->set('change_messages',$this->getChangeMessages());
 			$this->set('area',0);
@@ -130,6 +134,27 @@ class AreasController extends AppController {
 			}
 		}
 	}
+
+	function printm($id = null) {
+		$output = '';
+		if (!empty($this->data)) {
+			foreach($this->data['Area']['area_id'] as $id) {
+				$output .= $this->requestAction(
+					array('controller'=>'areas','action'=>'schedule'),
+					array('pass'=>array($id),'return')
+				);
+			}
+			$this->set('output',$output);
+			$this->set('back',$this->referer());
+		} else {
+			$this->Area->recursive = -1;
+			$this->Area->order = 'name';
+			$this->set('areas',$this->Area->sFind('list'));
+			$this->data['Area']['area_id'] = array($id);
+			$this->render('select_print');
+		}	
+	}
+
 
 }
 ?>

views/areas/printm.ctp
@@ -1,0 +1,5 @@
+<?=$output?>
+<script type='text/javascript'>
+	window.print();
+	location.href = '<?=$this->base.$back?>';
+</script>

views/areas/schedule.ctp
@@ -3,8 +3,13 @@
 	echo "Redirecting...";
 	die;
 } ?>
-<?=$this->element('menu',array('area'=>$area['Area']['id']));?>
-<?=$this->element('schedule_message');?>
+<? if (isset($print)) { ?>
+	<div style='page-break-after:always'>
+<? } else { ?>
+	<?=$this->element('menu',array('area'=>$area['Area']['id']));?>
+	<div>
+<? } ?>
+<?= !isset($print) ? $this->element('schedule_message') : '';?>
 <? 
 if($area['Area']['id'] == 0) {
 	echo $this->element('splash');
@@ -14,4 +19,5 @@ if($area['Area']['id'] == 0) {
 	$this->set('title_for_layout', $area['Area']['name']." Schedule");
 }
 ?>
-<?=$this->element('dialog');?>
+</div>
+<?= !isset($print) ? $this->element('dialog') : '';?>

views/areas/select_print.ctp
@@ -1,0 +1,17 @@
+<?= $form->create('Area',array('type'=>'post','onsubmit'=>'wait()'));?>
+	<fieldset>
+ 		<legend><?php __('Print Area Schedules');?></legend>
+	<div class='tall left' id='areas' style='width:300px'>
+	<?=$form->input('area_id',array('label'=>false,'type'=>'select','multiple'=>'checkbox','options'=>$areas));?>
+	</div>
+	<hr/>
+	<div class='left'>
+	<?=$form->checkbox('check_all',array(
+		'onclick' => "checkAll('areas',this)"
+	));?>
+	<?=$form->label('check_all');?>
+	</div>
+	</fieldset>
+<?= $form->submit('Submit');?>
+<?php echo $form->end();?>
+<?=$this->element('message',array('default_field'=>''));?>

views/helpers/schedule.php
@@ -405,5 +405,11 @@ class ScheduleHelper extends AppHelper {
 		echo $View->element('hours_by',array('data'=>$this->hours_by));
 	}
 
+	function clearHours() {
+		$this->total_hours = array(
+			'total'=>0,'1'=>0,'2'=>0,'3'=>0,'4'=>0,'5'=>0,'6'=>0,'7'=>0);
+		$this->hours_by = array();
+	}
+
 }
 ?>

commit bc1c80499c46c589891467a2c86474345525abd6

fixed bug with gaps introduced in 13a0405f99...

controllers/people_controller.php
@@ -15,20 +15,20 @@ class PeopleController extends AppController {
 				$person = $this->Person->getGaps();
 			} else {
 				$person = $this->Person->getPerson($id);
-				if (!isset($this->params['requested'])) {
-					$this->set('change_messages',$this->getChangeMessages());
-					$this->Session->write('last_person',$id);
-					$this->Person->PersonnelNote->order = 'PersonnelNote.order asc';
-					$personnelNotes = $this->Person->PersonnelNote->findAllByPersonId($id);
-					$this->set('personnelNotes', Set::combine(
-						$personnelNotes,'{n}.PersonnelNote.id','{n}.PersonnelNote.note'));
-					$this->Person->OperationsNote->order = 'OperationsNote.order asc';
-					$personnelNotes = $this->Person->OperationsNote->findAllByPersonId($id);
-					$this->set('operationsNotes', Set::combine(
-					$personnelNotes,'{n}.OperationsNote.id','{n}.OperationsNote.note'));
-				} else {
-					$this->set('print',true);
-				}
+				$this->Session->write('last_person',$id);
+			}
+			if (!isset($this->params['requested'])) {
+				$this->set('change_messages',$this->getChangeMessages());
+				$this->Person->PersonnelNote->order = 'PersonnelNote.order asc';
+				$personnelNotes = $this->Person->PersonnelNote->findAllByPersonId($id);
+				$this->set('personnelNotes', Set::combine(
+					$personnelNotes,'{n}.PersonnelNote.id','{n}.PersonnelNote.note'));
+				$this->Person->OperationsNote->order = 'OperationsNote.order asc';
+				$personnelNotes = $this->Person->OperationsNote->findAllByPersonId($id);
+				$this->set('operationsNotes', Set::combine(
+				$personnelNotes,'{n}.OperationsNote.id','{n}.OperationsNote.note'));
+			} else {
+				$this->set('print',true);
 			}
 			$this->set('person',$person);		
 			$this->set('bounds', $this->getBounds());

views/elements/menu.ctp
@@ -176,7 +176,7 @@
 				'url' => array(
 					'controller' => 'people',
 					'action' => 'printm',
-					(isset($person)) ? $person : null 
+					(isset($person) && !isset($gaps)) ? $person : null 
 				),
 				'ajax'
 			),

views/elements/schedule_content.ctp
@@ -33,7 +33,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 		<td width="9" rowspan="2">
 			&nbsp;
 		</td> 
-		<td class="title" <?= (isset($person)) ? 'id="total_hours_'.$person['Person']['id'].'"' : '';?> width="144" rowspan="2">
+		<td class="title" <?= (isset($person)) ? 'id="total_hours_'. ($gaps? '' : $person['Person']['id']) .'"' : '';?> width="144" rowspan="2">
 			<?= (isset($area)) ?
 				$area[$request.'Area']['manager'] :
 				'';?>
@@ -64,7 +64,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 					$person['PeopleSchedules']['ResidentCategory']['name'],
 					array(
 						'operations' => array(
-							'url' => array('action'=>'category',$person['Person']['id']),
+							'url' => array('action'=>'category',isset($gaps)? '' : $person['Person']['id']),
 							'attributes'=>array(
 								'update'=>'dialog_content',
 								'complete'=>"openDialog('category_name')",
@@ -74,7 +74,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 							'ajax'
 						)
 					),
-					($this->params['isAjax'] || !$editable)
+					($this->params['isAjax'] || !$editable || isset($gaps))
 				);?>
 			<? } ?>
 				<br />
@@ -257,7 +257,7 @@ $notes = $gaps ? false : (isset($area) ? $area["{$request}Area"]['notes'] : $per
 			<?// now that the total hours are added up, sneak them in at the top
 			?>
 			<script type="text/javascript">
-				$('total_hours_<?=$person['Person']['id']?>').innerHTML = <?=$schedule->total_hours['total'];?>;
+				$('total_hours_<?= $gaps? '' : $person['Person']['id']?>').innerHTML = <?=$schedule->total_hours['total'];?>;
 			</script>
 		<? } ?>
 		<? if ($isOperations && $editable && !$request