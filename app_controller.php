3099756112e2a98d2a3cf2d5c5a5d66be4e12694
Jason Galuten <jason@galuten.com>
2011-03-22 12:01:56 -0700

    fixed which schedule displays when latest

--- app_controller.php
+++ app_controller.php
@@ -147,6 +147,7 @@ class AppController extends Controller {
 		$schedule = $this->Schedule->find('first',$params);
 		if (!$schedule) {
 			unset($params['conditions']['ScheduleGroup.end >']);
+			$params['order'][0] = 'ScheduleGroup.end desc';
 			$schedule = $this->Schedule->find('first',$params);
 		}
 		$this->Session->write('Schedule', $schedule['Schedule']);	
